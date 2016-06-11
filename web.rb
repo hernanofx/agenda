87ac865ba24d53fc933a7710f331a64c0d26a110
hernanofx <hernanofx@hotmail.com>
2016-06-11 16:01:01 -0300

    modificacion completa web.rb

--- web.rb
+++ web.rb
@@ -1,32 +1,35 @@
+require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
+require 'json'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/todo_list.db")
-  class Item
+class Item
   include DataMapper::Resource
   property :id, Serial
   property :content, Text, :required => true
   property :done, Boolean, :required => true, :default => false
   property :created, DateTime
-  end
+end
 DataMapper.finalize.auto_upgrade!
 
-get '/' do
+get '/?' do
   @items = Item.all(:order => :created.desc)
   redirect '/new' if @items.empty?
   erb :index
 end
 
-get '/new' do
+get '/new/?' do
   @title = "Add todo item"
   erb :new
 end
-post '/new' do
+
+post '/new/?' do
   Item.create(:content => params[:content], :created => Time.now)
   redirect '/'
 end
 
-post '/done' do
+post '/done/?' do
   item = Item.first(:id => params[:id])
   item.done = !item.done
   item.save
@@ -35,11 +38,12 @@ post '/done' do
   { :id => params[:id], :status => value }.to_json
 end
 
-get '/delete/:id' do
+get '/delete/:id/?' do
   @item = Item.first(:id => params[:id])
   erb :delete
 end
-post '/delete/:id' do
+
+post '/delete/:id/?' do
   if params.has_key?("ok")
     item = Item.first(:id => params[:id])
     item.destroy
