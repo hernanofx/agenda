5d09337f6d858d5188353edbd1bb17a60bfc3639
hernanofx <hernanofx@hotmail.com>
2016-06-11 16:06:40 -0300

    modificando las dependencias

--- web.rb
+++ web.rb
@@ -4,32 +4,31 @@ require 'data_mapper'
 require 'json'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/todo_list.db")
-class Item
+  class Item
   include DataMapper::Resource
   property :id, Serial
   property :content, Text, :required => true
   property :done, Boolean, :required => true, :default => false
   property :created, DateTime
-end
+  end
 DataMapper.finalize.auto_upgrade!
 
-get '/?' do
+get '/' do
   @items = Item.all(:order => :created.desc)
   redirect '/new' if @items.empty?
   erb :index
 end
 
-get '/new/?' do
+get '/new' do
   @title = "Add todo item"
   erb :new
 end
-
-post '/new/?' do
+post '/new' do
   Item.create(:content => params[:content], :created => Time.now)
   redirect '/'
 end
 
-post '/done/?' do
+post '/done' do
   item = Item.first(:id => params[:id])
   item.done = !item.done
   item.save
@@ -38,12 +37,11 @@ post '/done/?' do
   { :id => params[:id], :status => value }.to_json
 end
 
-get '/delete/:id/?' do
+get '/delete/:id' do
   @item = Item.first(:id => params[:id])
   erb :delete
 end
-
-post '/delete/:id/?' do
+post '/delete/:id' do
   if params.has_key?("ok")
     item = Item.first(:id => params[:id])
     item.destroy
@@ -52,3 +50,4 @@ post '/delete/:id/?' do
     redirect '/'
   end
 end
+Status
